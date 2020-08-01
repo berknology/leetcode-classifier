df1faccd09d2caf604f089b8e8f5e874819d3fee
HH <berknology@gmail.com>
2020-07-31 21:12:53 -0700

    added solution for Problem 131

---
+++ leetcode_python_solutions/131_palindrome_partitioning.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        ans = []
+        self.backtrack(s, 0, [], ans)
+        return ans
+
+    def backtrack(self, s, index, path, ans):
+        if index == len(s):
+            ans.append(path)
+            return
+        for i in range(index, len(s)):
+            sub_str = s[index:i+1]
+            if sub_str[::-1] != sub_str:
+                continue
+            self.backtrack(s, i+1, path+[sub_str], ans)
