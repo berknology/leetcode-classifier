6fcb36220521a59834998e5825fce68342f78523
HH <berknology@gmail.com>
2020-08-08 17:54:47 -0700

    Added solution for Problem 997

---
+++ leetcode_python_solutions/997_find_the_town_judge.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        in_deg = [0]*N
+        out_deg = [0]*N
+        for pair in trust:
+            out_deg[pair[0]-1] += 1
+            in_deg[pair[1]-1] += 1
+        for i in range(N):
+            if in_deg[i] == N-1 and out_deg[i] == 0:
+                return i+1
+        return -1
+
+
+"""
+Time complexity: O(N+M) where M is the length of trust array
+Space complexity: O(N)
+"""
