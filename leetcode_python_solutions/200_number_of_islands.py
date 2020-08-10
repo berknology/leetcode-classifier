cc60dd643abdd67ab5d9438215439424ac2e013b
HH <berknology@gmail.com>
2020-08-10 10:16:21 -0700

    Added solution for Problem 200

---
+++ leetcode_python_solutions/200_number_of_islands.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]: return 0
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j, m, n)
+                    ans += 1
+        return ans
+
+    def dfs(self, grid, i, j, m, n):
+        grid[i][j] = '0'
+        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+            if 0 <= x < m and 0 <= y < n and grid[x][y] == '1':
+                self.dfs(grid, x, y, m, n)
+
+
+"""
+Time complexity: O(mn)
+Space complexity: O(mn)
+"""
