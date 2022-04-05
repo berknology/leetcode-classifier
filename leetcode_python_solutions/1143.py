afb21a7ffcac6bb8571915683f5836073e1a81c6
He Hao <hehao@microsoft.com>
2022-04-05 13:07:30 -0700

    add another solution for LCS

--- leetcode_python_solutions/1143.py
+++ leetcode_python_solutions/1143.py
@@ -9,3 +9,19 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         return dp[-1][-1]
+
+
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         return self.lcs(text1, text2, len(text1) - 1, len(text2) - 1, {})
+#
+#     def lcs(self, text1, text2, i, j, memo):
+#         if i < 0 or j < 0:
+#             return 0
+#         if (i, j) not in memo:
+#             if text1[i] == text2[j]:
+#                 memo[(i, j)] = 1 + self.lcs(text1, text2, i - 1, j - 1, memo)
+#             else:
+#                 memo[(i, j)] = max(self.lcs(text1, text2, i, j - 1, memo), self.lcs(text1, text2, i - 1, j, memo))
+#
+#         return memo[(i, j)]
