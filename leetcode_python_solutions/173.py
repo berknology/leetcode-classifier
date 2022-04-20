0cd943313560a22957bde78021665276dc72fe6a
He Hao <hehao@microsoft.com>
2022-04-20 14:51:35 -0700

    add solution to 173

---
+++ leetcode_python_solutions/173.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        self.traverse_left(root)
+
+    def traverse_left(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        node = self.stack.pop()
+        self.traverse_left(node.right)
+        return node.val
+
+    def hasNext(self) -> bool:
+        return len(self.stack) > 0
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
