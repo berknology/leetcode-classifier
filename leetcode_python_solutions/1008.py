ac3446a78a18a4c6fe380307d19e6f7edca4b216
He Hao <hehao@microsoft.com>
2022-08-07 14:47:17 -0700

    update solution to 1008

--- leetcode_python_solutions/1008.py
+++ leetcode_python_solutions/1008.py
@@ -5,20 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minCameraCover(self, root: TreeNode) -> int:
-        self.ans = 0
-        covered = {None}
-        self.dfs(root, covered, None)
-        return self.ans
-    
-    def dfs(self, root, covered, par):
-        if not root:
-            return
-        self.dfs(root.left, covered, root)
-        self.dfs(root.right, covered, root)
-        if (root.left not in covered or root.right not in covered) or (not par and root not in covered):
-            self.ans += 1
-            covered.add(root.left)
-            covered.add(root.right)
-            covered.add(root)
-            covered.add(par)
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        preorder.reverse()
+        return self.construct_bst(preorder, float('-inf'), float('inf'))
+
+    def construct_bst(self, preorder, lower, upper):
+        if not preorder:
+            return None
+        if lower < preorder[-1] < upper:
+            root = TreeNode(preorder.pop())
+            root.left = self.construct_bst(preorder, lower, root.val)
+            root.right = self.construct_bst(preorder, root.val, upper)
+            return root
